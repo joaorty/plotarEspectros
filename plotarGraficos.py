598e763f2c11a2711a96e66118b771bee1e6c812
joaorty <57726000+joaorty@users.noreply.github.com>
2021-09-27 17:54:45 -0300

    Add files via upload

---
+++ plotarGraficos.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def desenharGrafico(titulo): 
+  plt.xlabel("Comprimento de Onda [Å]")
+  plt.ylabel("Intensidade [erg/sec/cm²]")
+  plt.title(titulo)
+  plt.show()
+
+def plotar(titulo):
+  x, y = np.loadtxt(titulo, delimiter = "  ", unpack = True)
+  plt.plot(x, y, "k", linewidth=0.5)
+  desenharGrafico(titulo)
+
+def plotarTudo(titulos):
+  for tituloAtual in titulos:
+    plotar(tituloAtual)
+
+arquivos = ["f1240_4381.dat", "f1245_888.dat", "f1308_5325.dat", "f1329_3262.dat", "f1558_4020.dat", "f1625_1688.dat"]
+
+for i in range(len(arquivos)):
+  print((i+1), "-", arquivos[i])
+
+n = int(input("Digite qual arquivo você quer plotar: "))
+plotar(arquivos[n-1])
